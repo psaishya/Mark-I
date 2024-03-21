4b2cee05c1fb3bf30ff85fb56dc7b32e2795368f
sitjan <sitaulajan@gmail.com>
2024-03-21 23:00:18 +0545

    streaming feature added

--- trial2/server.js
+++ trial2/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Socket } = require('socket.io');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
@@ -8,6 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+  console.log(req.query);
   res.redirect(`/${uuidV4()}`);
 });
 
@@ -15,4 +17,10 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
 });
 
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+  });
+});
 server.listen(3000);
